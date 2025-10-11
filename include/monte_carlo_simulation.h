e19df8868b7bf65419805a115aca3f3901544db2
J Gibson <jg535@student.le.ac.uk>
2025-10-11 17:30:04 +0100

    Implemented unoptimized monte carlo simulation

---
+++ include/monte_carlo_simulation.h
@@ -0,0 +1,15 @@
+
+
+
+#ifndef FINAL_YEAR_PROJECT_MONTE_CARLO_SIMULATION_H
+#define FINAL_YEAR_PROJECT_MONTE_CARLO_SIMULATION_H
+#include <vector>
+std::vector<double> generateRandomNormalVariables(int no_of_paths);
+
+double calculatePayOff(double terminal_price,double strike_price);
+
+double simulatePayOffs(int no_of_paths,double terminal_prices[],double strike_price);
+
+double runSimulation(int no_of_paths,double spotPrice , double strikePrice , double timeToMaturity,double riskFreeRate , double volatility);
+
+#endif //FINAL_YEAR_PROJECT_MONTE_CARLO_SIMULATION_H
--- src/monte_carlo_simulation.cpp
+++ src/monte_carlo_simulation.cpp
@@ -2,8 +2,7 @@
 #include <cmath>
 #include <bits/stdc++.h>
 #include <random>
-
-
+#include "monte_carlo_simulation.h"
 
 std::vector<double> generateRandomNormalVariables(int no_of_paths) {
 
@@ -16,6 +15,7 @@ std::vector<double> generateRandomNormalVariables(int no_of_paths) {
     for (int i=0;i<no_of_paths;i++) {
         randomNumbers[i] = normal_dist(generator);
     }
+
 
     return randomNumbers;
 
@@ -41,12 +41,14 @@ double runSimulation(int no_of_paths,double spotPrice , double strikePrice , dou
 
     for (int i = 0;i<no_of_paths;i++) {
 
-        terminal_prices[i] = spotPrice * std::exp((riskFreeRate - (0.5 * std::pow(volatility,2.0)) * timeToMaturity + (volatility*std::sqrt(timeToMaturity)*randomNumbers[i])));
+        terminal_prices[i] = spotPrice * std::exp(
+            (riskFreeRate - 0.5 * std::pow(volatility,2.0))
+            * timeToMaturity + volatility*std::sqrt(timeToMaturity)*randomNumbers[i]);
      }
 
     double sum = simulatePayOffs(no_of_paths,terminal_prices,strikePrice);
 
-    double estimated_value = std::exp(-riskFreeRate*timeToMaturity) * (1/no_of_paths) * sum;
+    double estimated_value = std::exp(-riskFreeRate*timeToMaturity) * (1.0/no_of_paths) * sum;
 
 
     // need to change to call functions and return the estimated value of option
