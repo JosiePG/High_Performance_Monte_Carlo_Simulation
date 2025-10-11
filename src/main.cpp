cc1bcb42f36e06a98ffe2ff1626ea845430f25ff
J Gibson <jg535@student.le.ac.uk>
2025-10-11 15:25:39 +0100

    Added basic user input for option metrics in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Admin on 11/10/2025.
-//
+#include <iostream>
+
+int main() {
+    double spotPrice;
+    double strikePrice;
+    double timeToMaturity; // Measured in years
+    double riskFreeRate;
+    double volatility;
+
+    std::cout << "Monte Carlo Simulation For Option Pricing!" << std::endl;
+
+    std::cout << "Enter spot price : "<< std::endl;
+    std::cin >> spotPrice;
+    std::cout << "Enter strike price : "<< std::endl;
+    std::cin >> strikePrice;
+    std::cout << "Enter time to maturity : "<< std::endl;
+    std::cin >> timeToMaturity;
+    std::cout << "Enter risk free rate: "<< std::endl;
+    std::cin >> riskFreeRate;
+    std::cout << "Enter volatility : "<< std::endl;
+    std::cin >> volatility;
+
+}
